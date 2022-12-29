fa3b4d148236af516109346e82b3579f6113b790
Connor Skees <connor1skees@gmail.com>
2022-12-29 00:43:26 -0500

    use monaco editor

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 const js = import("../node_modules/@connorskees/grass/grass.js");
 
-window.doRepl = () => {
-  let textIn = document.getElementById("in").value;
-  let out = document.getElementById("out");
+window.compile = (input, output) => {
+  if (!input || !output) {
+    return;
+  }
+
+  let inText = input.getModel().getValue();
+  let currentOutText = output.getModel().getValue();
+
   js.then((js) => {
+    let outText;
     try {
-      out.value = js.from_string(textIn);
+      let compiled = js.from_string(inText);
+      if (compiled === currentOutText) {
+        return;
+      }
+
+      outText = compiled;
     } catch (e) {
-      out.value = e;
+      if (e === currentOutText) {
+        return;
+      }
+
+      outText = e;
     }
+
+    output.getModel().setValue(outText);
   });
 };
-
-setInterval(window.doRepl, 100);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 module.exports = {
-  entry: "./index.js",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
